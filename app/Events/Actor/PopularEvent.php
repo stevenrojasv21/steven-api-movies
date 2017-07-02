b240b8e354e139bbefbfcf2f8babecdffaa86802
stevenrojasv21 <stevenrojasv21@gmail.com>
2017-07-01 19:18:17 -0500

    Revert "Dev feature/structure response"

--- app/Events/Actor/PopularEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Events\Actor;
-
-use App\Events\CommonEvent as Event;
-
-class PopularEvent extends Event
-{
-}
--- app/Events/Multi/IndexEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Events\Multi;
-
-use App\Events\CommonEvent as Event;
-
-class IndexEvent extends Event
-{
-}
